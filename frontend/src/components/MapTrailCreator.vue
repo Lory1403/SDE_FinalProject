2f2ec06501ae1b0b300c61cf8c7a752e25fb5c24
Bigceru <davide.ceru02@gmail.com>
2025-01-31 15:53:52 +0100

    Implemented some CSS and added code comments. Fixed 'save trail' button (it was enabled also with no track drawn on map)

--- frontend/src/components/MapTrailCreator.vue
+++ frontend/src/components/MapTrailCreator.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="container">
+    <label class="waiting-label" v-if="map == null">Loading map...</label>
     <div id="map" ref="map"></div>
     <div class="control-container">
-      <button :disabled="!selectedPoints || selectedPoints.length !== 2" @click="calculateTrail">Calculate Trail</button>
-      <button :disabled="selectedPoints.length == 0" @click="clearMap">Clear Map</button>
-      <button :disabled="!trackCoordinates" @click="saveTrack">Save Trail</button>
+      <button class="button-type" :disabled="!selectedPoints || selectedPoints.length !== 2" @click="calculateTrail">Calculate
+        Trail</button>
+      <button class="button-type" :disabled="selectedPoints.length == 0" @click="clearMap">Clear Map</button>
+      <button class="button-type" :disabled="!trackCoordinates" @click="saveTrack">Save Trail</button>
     </div>
   </div>
 </template>
@@ -17,11 +19,11 @@ export default {
   data() {
     return {
       map: null,
-      marker1: null,
-      marker2: null,
-      selectedPoints: [],
-      trackLayer: null,
-      trackCoordinates: null
+      marker1: null,                  // Marker for the first selected point
+      marker2: null,                  // Marker for the second selected point
+      selectedPoints: [],             // Array to store the selected point coordinates
+      trackLayer: null,               // Layer to store the track polyline
+      trackCoordinates: null          // Array to store the track coordinates
     };
   },
   mounted() {
@@ -29,24 +31,31 @@ export default {
   },
   methods: {
     initMap() {
+      // Check if geolocation is supported by the browser
       if (navigator.geolocation) {
+        // Get the current position of the user
         navigator.geolocation.getCurrentPosition((position) => {
           const { latitude, longitude } = position.coords;
+
+          // Set the map view to the user's current position
           this.map = L.map(this.$refs.map).setView([latitude, longitude], 13);
           L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           }).addTo(this.map);
 
+          // Add a click event listener to the map
           this.map.on('click', (e) => {
+            // If no points are selected, add the first marker
             if (!this.selectedPoints.length) {
               this.marker1 = L.marker(e.latlng).addTo(this.map);
               this.selectedPoints.push(e.latlng);
-            } else if (this.selectedPoints.length === 1) {
+            } else if (this.selectedPoints.length === 1) {        // If one point is already selected, add the second marker
               this.marker2 = L.marker(e.latlng).addTo(this.map);
               this.selectedPoints.push(e.latlng);
             }
           });
-        }, () => {
+        }, 
+        () => {
           // Fallback in case of error or if the user denies geolocation
           this.map = L.map(this.$refs.map).setView([45.5236, -122.6750], 13);
           L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -61,17 +70,21 @@ export default {
         }).addTo(this.map);
       }
     },
+
+    // Function to clear the selected points and markers
     selectPoints() {
-        this.selectedPoints = [];
-        if (this.marker1) {
-            this.map.removeLayer(this.marker1);
-            this.marker1 = null;
-        }
-        if (this.marker2) {
-            this.map.removeLayer(this.marker2);
-            this.marker2 = null;
-        }
+      this.selectedPoints = [];
+      if (this.marker1) {
+        this.map.removeLayer(this.marker1);
+        this.marker1 = null;
+      }
+      if (this.marker2) {
+        this.map.removeLayer(this.marker2);
+        this.marker2 = null;
+      }
     },
+
+    // Function to calculate the track between the selected points
     calculateTrail() {
       if (this.selectedPoints.length < 2) {
         console.error('Not enough points selected to calculate trail');
@@ -87,33 +100,38 @@ export default {
 
       console.log(url);
 
+      // Call the API to calculate the trail using selected points
       fetch(url)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(data => {
+          this.trackCoordinates = data.geometry.coordinates;  // Store the track coordinates
+          this.drawTrack();                                   // Draw the track on the map
+        })
+        .catch(error => {
+          console.error('Error calculating trail:', error);
+        });
+    },
+
+    // Function to draw the track on the map
+    drawTrack() {
+      if (this.trackCoordinates && this.trackCoordinates.length > 0) {
+        const latlngs = this.trackCoordinates.map(coord => [coord[1], coord[0]]); // Inverti latitudine e longitudine
+
+        if (this.trackLayer) {
+          this.map.removeLayer(this.trackLayer);
         }
-        return response.json();
-      })
-      .then(data => {
-        this.trackCoordinates = data.geometry.coordinates;
-        this.drawTrack();
-      })
-      .catch(error => {
-        console.error('Error calculating trail:', error);
-      });
-  },
-  drawTrack() {
-    if (this.trackCoordinates && this.trackCoordinates.length > 0) {
-      const latlngs = this.trackCoordinates.map(coord => [coord[1], coord[0]]); // Inverti latitudine e longitudine
 
-      if (this.trackLayer) {
-        this.map.removeLayer(this.trackLayer);
+        this.trackLayer = L.polyline(latlngs, { color: 'blue' }).addTo(this.map);
+        this.map.fitBounds(this.trackLayer.getBounds());
       }
+    },
 
-      this.trackLayer = L.polyline(latlngs, { color: 'blue' }).addTo(this.map);
-      this.map.fitBounds(this.trackLayer.getBounds());
-    }
-  },
+    // Function to clear all added map elements
     clearMap() {
       this.selectedPoints = [];
       if (this.marker1) {
@@ -128,7 +146,11 @@ export default {
         this.map.removeLayer(this.trackLayer);
         this.trackLayer = null;
       }
+
+      this.trackCoordinates = null;
     },
+
+    // Function to store the track on the database
     saveTrack() {
       axios.post('/api/tracks/save', { coordinates: this.trackCoordinates })
         .then(response => {
@@ -140,19 +162,49 @@ export default {
 </script>
 
 <style>
-#map {
-    margin: 0 auto;
-    border-radius: 8px;
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-    height: 500px;
-    width: 1000px;
-
-    /* Positioning parameters */
-    position: relative;
-    left: 0%;
-    top: -0px;
+/* Keyframes for animating the waiting label */
+@keyframes fadeInOut {
+  0%, 100% { opacity: 0; }
+  50% { opacity: 1; }
 }
+
+/* Styling for the waiting label */
+.waiting-label {
+  display: block;
+  text-align: center;
+  margin-top: 10px;
+  font-size: 24px;
+  animation: fadeInOut 4s infinite;
+}
+
+/* Styling for the map container */
+#map {
+  margin: 0 auto;
+  border-radius: 8px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  height: 600px;
+  /* width: 1000px; */
+
+  /* Positioning parameters */
+  position: relative;
+  left: 0%;
+  top: -0px;
+}
+
+/* Styling for the control container (buttons) */
 .control-container {
   margin-top: 20px;
+}
+
+.button-type {
+  padding: 10px 20px;
+  font-size: 16px;
+  background-color: #4285f4;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  margin: 0 5px;
+  transition: background-color 0.3s ease;
 }
 </style>
